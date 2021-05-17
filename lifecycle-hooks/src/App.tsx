7ea8df72a9e1b383635f0a01fcde575b7f645a96
Samba <samba.pendela@gmail.com>
2021-05-17 10:01:26 +0530

    react life cycle hooks

---
+++ lifecycle-hooks/src/App.tsx
@@ -0,0 +1,71 @@
+import  React, { Component } from "react";
+import Child from "./Child";
+
+interface IState{
+   "key1" : string,
+   "key2" : any
+}
+
+interface IProps{}
+
+class App extends Component<IProps,IState>{
+
+
+      //when ever we create the component, automatically "constructor" will execute
+      //constructor will execute "only" once
+      //in constructor generally we will receive "props" and we can initilize "state" also
+      constructor(props:IProps){
+        console.log("parent constructor");
+        super(props);
+        this.state = {
+          "key1" : "Hello_1",
+          "key2" : ""
+        }
+      };
+
+
+      //some times we may change the state, based on state & props
+      //if you want to change state after constructor, then recomended life cycle hook is componentWillMount()
+      //it's recomended to change state here by using setState() function
+      //in general we will configure global parameters like "width" and "height" also
+      componentWillMount(){
+        console.log("parent componentWillMount");
+        if(window.innerWidth<500){
+          this.setState({
+              key2 : window.innerWidth
+          })
+        }
+      }
+
+
+
+
+      //after componentWillMount() automatically render() life cycle hook will execute
+      //this life cycle hook is "mandatory" life cycle hook
+      //in general we will place "presentation" logic here
+      //when ever change detected in state, automatically "render()" function will execute
+      //never use "setState()" function here
+      render(){
+          console.log("parent render")
+          return(
+            <React.Fragment>
+                {this.state.key1} <br></br><br></br>
+                Inner Width....{this.state.key2}
+                <br></br>
+                <Child key1={this.state.key1}></Child>
+            </React.Fragment>
+          )
+      };
+
+
+      //after render() life cycle hook, automatically componentDidMount() life cycle hook will execute
+      //in general, in componentDidMount() will make rest api calls
+      //in general, we will change state also here based on rest api response
+      componentDidMount(){
+          console.log("parent componentDidMount()");
+      }
+
+
+};
+
+export default App;
---
+++ lifecycle-hooks/src/Child.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+interface IState{}
+
+interface IProps{
+    key1:any
+}
+
+class Child extends Component<IProps,IState>{
+    constructor(props:IProps){
+        console.log("child constructor");
+        super(props);
+    }
+    componentWillMount(){
+        console.log("child componentWillMount");
+    }
+    render(){
+        console.log("child render");
+        return(
+            <React.Fragment>
+                {this.props.key1}
+            </React.Fragment>
+        )
+    };
+    componentDidMount(){
+        console.log("child componentDidMount")
+    }
+};
+
+export default Child;
+
+
+//life cycle hooks
+//HOC  (Higher Order Components)
+//pure components
+//jest
+
+
+//GitHub
+
+
+
+
